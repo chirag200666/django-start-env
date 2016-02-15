e4f242acf63cd03c56923245fda9785a996e0985
Chirag Choudhary <chirag200666@gmail.com>
2016-02-15 17:11:11 +0530

    removing script location folder from settings

--- start_django_dev.sh
+++ start_django_dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ./shell_script/settings.cfg
-
-guake -n 'whatever' -r 'redis' -e 'sh '$SCRIPT_FOLDER'/start_redis_server.sh'
-guake -n 'whatever' -r 'local shell' -e '. '$SCRIPT_FOLDER'/start_django_shell.sh '$DJANGO_PROJECT' '$VIRTUAL_ENV
-guake -n 'whatever' -r 'local run' -e '. '$SCRIPT_FOLDER'/start_django_server.sh '$DJANGO_PROJECT' '$VIRTUAL_ENV
-guake -n 'whatever' -r 'log file' -e '. '$SCRIPT_FOLDER'/start_django_log.sh '$DJANGO_PROJECT
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )""/shell_script"
+guake -n 'whatever' -r 'redis' -e 'sh '$DIR'/start_redis_server.sh'
+guake -n 'whatever' -r 'local shell' -e '. '$DIR'/start_django_shell.sh '$DJANGO_PROJECT' '$VIRTUAL_ENV
+guake -n 'whatever' -r 'local run' -e '. '$DIR'/start_django_server.sh '$DJANGO_PROJECT' '$VIRTUAL_ENV
+guake -n 'whatever' -r 'log file' -e '. '$DIR'/start_django_log.sh '$DJANGO_PROJECT
