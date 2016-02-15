d364094400f06998054d81b2dba4d4d93ada421f
Chirag Choudhary <chirag200666@gmail.com>
2016-02-15 17:00:55 +0530

    adding virtual env location in settings file

--- shell_script/start_django_shell.sh
+++ shell_script/start_django_shell.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-#cd '/home/chirag/Desktop/django/kaddy_main/repo/kaddy'
-echo $0 "switching to directory" $1
+#echo $0 "switching to directory" $1
 cd $1
+
+if [ -n "$2" ]; then
+    echo "Using Virtual environment set form the settings.cfg."$2"/bin/activate"
+    source $2"/bin/activate"
+else
+    echo "Virtual environment not set"
+fi
 
 PYTHONSTARTUP=imports.py python ./manage.py shell --plain
--- start_django_dev.sh
+++ start_django_dev.sh
@@ -2,6 +2,6 @@
 source ./shell_script/settings.cfg
 
 guake -n 'whatever' -r 'redis' -e 'sh '$SCRIPT_FOLDER'/start_redis_server.sh'
-guake -n 'whatever' -r 'local shell' -e '. '$SCRIPT_FOLDER'/start_django_shell.sh '$DJANGO_PROJECT
+guake -n 'whatever' -r 'local shell' -e '. '$SCRIPT_FOLDER'/start_django_shell.sh '$DJANGO_PROJECT' '$VIRTUAL_ENV
 guake -n 'whatever' -r 'local run' -e '. '$SCRIPT_FOLDER'/start_django_server.sh'
 guake -n 'whatever' -r 'log file' -e '. '$SCRIPT_FOLDER'/start_django_log.sh'
